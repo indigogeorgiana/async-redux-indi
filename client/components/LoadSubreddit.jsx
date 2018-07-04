1bf09353d4f6a4243c6e1b988e9e65911c68618b
Indigo Phillips <indigogeorgiana@gmail.com>
2018-07-05 08:04:43 +1200

    Added input field

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,10 +2,30 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
-const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+class LoadSubreddit extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      country: ''
+    }
+    this.changeCountry = this.changeCountry.bind(this)
+  }
+
+  changeCountry (event) {
+    this.setState({
+      country: event.target.value
+
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <input onChange={this.changeCountry}/>
+        <button onClick={() => this.props.dispatch(fetchPosts(this.state.country))}>Submit</button>
+      </div>
+    )
+  }
+}
 
 export default connect()(LoadSubreddit)
--- client/components/SubredditList.jsx
+++ client/components/SubredditList.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import Moment from 'react-moment';
 
 import Post from './Post'
 
@@ -7,9 +8,18 @@ const Subreddit = ({subreddits}) => (
   <div>
     {subreddits.map((post, i) =>
       <Post
+  
         key={i}
+        // title
         title={post.title}
+        // date
+        date= {d.toDateString()}
+        // time
+        time={post.created_utc}.toUTCString()
+        // summary
+        selftext:{post.selftext}
       />
+
     )}
   </div>
 )
